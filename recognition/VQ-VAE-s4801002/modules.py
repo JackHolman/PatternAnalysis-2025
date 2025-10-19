1adbcf03494e3cf5ce8f56a46c36dc4deb72375b
Jack Holman <53689933+JackHolman@users.noreply.github.com>
2025-10-19 12:49:59 +1000

    Alter model structure to support HipMRIStudy data

--- recognition/VQ-VAE-s4801002/modules.py
+++ recognition/VQ-VAE-s4801002/modules.py
@@ -14,7 +14,7 @@ class VectorQuantiser(nn.Module):
         self._embeddings.weight.data.uniform_(-1/embedding_num, 1/embedding_num)
 
     def forward(self, inputs, disp_unique):
-        # Input is (B, C=self._embedding_dim, W=4, H=4)
+        # Input is (B, C=self._embedding_dim, W=4, H=8)
         inputs = inputs.permute(0, 2, 3, 1).contiguous()
         inputs_flattened = inputs.view(-1, self._embedding_dim)
 
@@ -53,9 +53,9 @@ class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
         self._block = nn.Sequential(
-            nn.ReLU(True),
+            nn.LeakyReLU(True),
             nn.Conv2d(channels, channels, 3, 1, 1),
-            nn.ReLU(True),
+            nn.LeakyReLU(True),
             nn.Conv2d(channels, channels, 1)
         )
 
@@ -84,17 +84,23 @@ class Encoder(nn.Module):
     def __init__(self, embedding_dim):
         super(Encoder, self).__init__()
 
-        # Input is (B, C=1, W=64, H=64)
+        # Input is (B, C=1, W=128, H=256)
         self._encoder_conv = nn.Sequential(
-            DownSampleBlock(1, 32, 3, 1, 1), # (B, C=32, W=32, H=32)
-            DownSampleBlock(32, 64, 3, 1, 1),  # (B, C=64, W=16, H=16)
-            DownSampleBlock(64, 128, 3, 1, 1),  # (B, C=128, W=8, H=8)
+            DownSampleBlock(1, 32, 3, 1, 1), # (B, C=32, W=64, H=128)
+            ResidualBlock(32),  # Size Unchanged
+            DownSampleBlock(32, 64, 3, 1, 1),  # (B, C=64, W=32, H=64)
+            ResidualBlock(64),  # Size Unchanged
+            DownSampleBlock(64, 128, 3, 1, 1),  # (B, C=128, W=16, H=32)
             ResidualBlock(128), # Size Unchanged
-            DownSampleBlock(128, 256, 3, 1, 1),  # (B, C=256, W=4, H=4)
+            DownSampleBlock(128, 256, 3, 1, 1),  # (B, C=256, W=8, H=16)
             ResidualBlock(256), # Size Unchanged
+            DownSampleBlock(256, 512, 3, 1, 1),  # (B, C=512, W=4, H=8)
+            ResidualBlock(512),  # Size Unchanged
+            ResidualBlock(512),  # Size Unchanged
 
             # Now compress into embedding space.
-            nn.Conv2d(256, embedding_dim, 3, 1, 1) # (B, C=embedding_dim, W=4, H=4)
+            nn.Conv2d(512, embedding_dim, 1, 1, 0), # (B, C=embedding_dim, W=4, H=8)
+            nn.BatchNorm2d(embedding_dim)
         )
 
     def forward(self, inputs):
@@ -120,13 +126,15 @@ class Decoder(nn.Module):
 
         self._decoder_block = nn.Sequential(
             # 1x1 convolution to increase number of channels
-            nn.ConvTranspose2d(embedding_dim, 256, kernel_size=1), # (B, C=256, W=4, H=4)
+            nn.ConvTranspose2d(embedding_dim, 512, kernel_size=1), # (B, C=512, W=4, H=8)
 
-            UpSampleBlock(256, 128, 3, 2, 1, 1), # (B, C=128, W=8, H=8)
-            UpSampleBlock(128, 64, 3, 2, 1, 1),  # (B, C=64, W=16, H=16)
-            UpSampleBlock(64, 32, 3, 2, 1, 1), # (B, C=32, W=32, H=32)
+            UpSampleBlock(512, 256, 3, 2, 1, 1), # (B, C=256, W=8, H=16)
+            ResidualBlock(256),  # Size Unchanged
+            UpSampleBlock(256, 128, 3, 2, 1, 1),  # (B, C=128, W=16, H=32)
+            UpSampleBlock(128, 64, 3, 2, 1, 1), # (B, C=64, W=32, H=64)
+            UpSampleBlock(64, 32, 3, 2, 1, 1),  # (B, C=32, W=64, H=128)
 
-            nn.ConvTranspose2d(32, 1, 3, 2, 1, 1), # (B, C=1, W=64, H=64)
+            nn.ConvTranspose2d(32, 1, 3, 2, 1, 1), # (B, C=1, W=128, H=256)
             nn.Sigmoid()
         )
 
@@ -136,8 +144,8 @@ class Decoder(nn.Module):
 class VQVAE(nn.Module):
     def __init__(self):
         super(VQVAE, self).__init__()
-        embedding_dim = 64
-        embedding_num = 256
+        embedding_dim = 128
+        embedding_num = 512
 
         self._encoder = Encoder(embedding_dim)
         self._quantiser = VectorQuantiser(embedding_num, embedding_dim)
